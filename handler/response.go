4b4facf59b7137aa90298c78e0f1a79ddc97a04b
ViktorG13 <eduardoviktor0@outlook.com>
2024-02-04 17:46:56 -0300

    :books: docs: Add Swagger Response Types

--- handler/response.go
+++ handler/response.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/ViktorG13/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,4 +23,31 @@ func sendSuccess(ctx *gin.Context, opr string, data interface{}) {
 		"message": fmt.Sprintf("Operation: %s Successfully", opr),
 		"data":    data,
 	})
+}
+
+type ErrorResponse struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+type CreateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type DeleteOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type ListOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type ShowOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
+type UpdateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
 }
